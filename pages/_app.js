a49484d395cc230f7dfff460a56d0e577af1e894
u18335412@tuks.co.za <u18335412@tuks.co.za>
2021-10-30 20:47:38 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import 'tailwindcss/tailwind.css'
-
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Static Job Posting</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Spartan:wght@500;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        desaturatedCyan: "hsl(180, 29%, 50%)",
+        lightGrayishCyanBackground: "hsl(180, 52%, 96%)",
+        lightGrayishCyanFilterTablets: "hsl(180, 31%, 95%)",
+        darkGrayishCyan: "hsl(180, 8%, 52%)",
+        veryDarkishCyan: "hsl(180, 14%, 20%)",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+  
+    },
   },
   plugins: [],
-}
+};
